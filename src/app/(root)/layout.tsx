5f2976d1b0831276f2ea9fb5ba3a762a539c33d1
vasyaqwe <vasyaqwe>
2023-10-21 20:17:09 +0300

    test: trying to make chats list refresh in production

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -19,6 +19,7 @@ export const metadata: Metadata = {
 
 export const dynamic = "force-dynamic"
 export const fetchCache = "force-no-store"
+export const revalidate = 0
 
 export default async function RootLayout({
     children,
