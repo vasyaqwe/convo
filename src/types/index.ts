f4a4a4839e42bee3bedcb1c43b5a743fd274d8e4
vasyaqwe <vasyaqwe>
2023-10-24 16:32:50 +0300

    chore: refactor types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,13 @@
 import { Chat, Message, User } from "@prisma/client"
 
-export type UserType = Pick<User, "name"> &
-    Pick<User, "username"> &
-    Pick<User, "id">
+export type UserType = Pick<User, "name" | "username" | "id" | "image">
 
 export type ExtendedMessage = Message & {
     sender: UserType
     seenBy: UserType[]
     displaySender?: boolean
 }
+
 export type ExtendedChat = Chat & {
     users: UserType[]
     messages: ExtendedMessage[] | undefined
