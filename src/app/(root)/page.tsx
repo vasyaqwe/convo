874179e6d0340ba57a643217704c817cd4b2fa35
vasyaqwe <vasyaqwe>
2023-10-31 18:46:08 +0200

    feat: add no-caching exports to /

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,6 +3,9 @@ import { MobileNav } from "@/components/layout/mobile-nav"
 import { getAuthSession } from "@/lib/auth"
 import { redirect } from "next/navigation"
 
+export const dynamic = "force-dynamic"
+export const fetchCache = "force-no-store"
+
 export default async function Home() {
     const session = await getAuthSession()
 
